7cf7351c11ba187ff0e0ae392514de5662277995
Rysanek <rysanekrivera@gmail.com>
2022-04-03 22:46:38 -0400

    River Layout Change

--- app/src/main/java/com/rysanek/feeditemconsumer/presentation/viewholders/RiverViewHolder.kt
+++ app/src/main/java/com/rysanek/feeditemconsumer/presentation/viewholders/RiverViewHolder.kt
@@ -3,6 +3,7 @@ package com.rysanek.feeditemconsumer.presentation.viewholders
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.rysanek.feeditemconsumer.data.local.entities.FeedItemEntity
 import com.rysanek.feeditemconsumer.data.remote.models.Component
 import com.rysanek.feeditemconsumer.databinding.LayoutRiverBinding
@@ -17,10 +18,14 @@ class RiverViewHolder(private val binding: LayoutRiverBinding): RecyclerView.Vie
                     false
                 )
             )
-        
     }
     
     fun bind(feedItem: FeedItemEntity) {
         binding.textView.text = feedItem.headline
+        Glide.with(binding.root.context)
+            .load("https://github.com/RysanekRivera/FeedItemConsumer/blob/master/river.jpg?raw=true")
+            .centerCrop()
+            .into(binding.ivRiverBackground)
+            
     }
 }
